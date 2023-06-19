8572ea201a984b8861c438b5800eeb1ecdd527d1
Raunit S <125693433+sraunit@users.noreply.github.com>
2023-06-19 18:11:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
+
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+app.use(bodyParser.json());
+
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: "https://ringer-d9eef-default-rtdb.firebaseio.com"
+});
+
+app.post('/', (req, res) => {
+	const payload = req.body;
+	console.log(payload);
+	res.status(200).send('Success')
 })
+
 app.listen(process.env.PORT || 3000)
